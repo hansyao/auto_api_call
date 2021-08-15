3ba8e3c1d7cbb6ad77aa5b7de43bb174a38daac1
hansyao <hansyow@gmail.com>
2021-08-16 01:36:02 +0800

    update enviroments variable

--- init_token.sh
+++ init_token.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
-CLIENT_ID='c4a6ecb7-caa0-4bf1-aef3-43f599714f19'
-CLIENT_SECRET='egG__p87.AI_Ca42ElbPRqjH9BQtoY44Wz'
+CLIENT_ID='xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxx'
+CLIENT_SECRET='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 TOKEN="$(mktemp)"
 rclone authorize "onedrive" "${CLIENT_ID}" "${CLIENT_SECRET}" \
 	| sed -n '/{/,/}/p' | sed '$d' | jq -r \
