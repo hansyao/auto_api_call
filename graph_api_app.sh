921b93be571167ce41de03c568ff229f011b76e1
hansyao <hansyow@gmail.com>
2021-08-17 12:55:40 +0800

    Update graph_api_app.sh

--- graph_api_app.sh
+++ graph_api_app.sh
@@ -80,8 +80,8 @@ function multi_process_kill() {
 	do
 		local THREADS=$(ps -ef | grep ${PROCESS_NAME} | grep -v grep | wc -l)
 		if [[ $[THREADS] -le 1 ]]; then break; fi
-		if [[ $[THREADS] -le 6 ]]; then
-			if [[ $[i] -le 5 ]]; then
+		if [[ $[THREADS] -le 2 ]]; then
+			if [[ $[i] -le 2 ]]; then
 				sleep 1
 				let i++
 				continue
