4bfd4137516d3f887486c61786557713e041396e
hansyao <hansyow@gmail.com>
2021-08-20 22:25:07 +0800

    update tencent function trigger

--- trigger.sh
+++ trigger.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-ACTION=$1
-TRIGGER_NAME=$2
-TRIGGER_DESC=$3
-REGION=${TENCENTCLOUD_REGION}
-VER='2018-04-16'
-ALGORITHM='TC3-HMAC-SHA256'
-TYPE="Content-Type:application/json"
-HOST='scf.tencentcloudapi.com'
-BODY="{\"FunctionName\": \"${SCF_FUNCTIONNAME}\", \
-\"TriggerName\": \"${TRIGGER_NAME}\", \
-\"Type\": \"timer\", \
-\"TriggerDesc\": \"${TRIGGER_DESC}\"}"
-
-SECRETID=${SECRET_ID}
-SECRETKEY=${SECRET_KEY}
-
 timestamp() {
 	if [ $1 -eq 1 ]; then
 		echo -e $(date -u +%s)
@@ -24,6 +8,26 @@ timestamp() {
 	else
 		echo -e $(date -u "+%F %H:%M:%S")
 	fi
+}
+
+env_var() {
+	source ./graphi_api_app.sh
+	local CLIENT_LIST=$(get_client_info)
+	if [[ -z ${CLIENT_LIST} ]]; then
+		echo "API账号未设置, 结束任务"
+		exit 0
+	fi
+	echo -e "${CLIENT_LIST}" | while read ACCOUNT && [[ -n "${ACCOUNT}" ]]
+	do
+		local NAME=$(echo -e "${ACCOUNT}" | awk '{print $1}' | sed s/"CLIENT_ID"//g)
+		local CLIENT_ID=$(echo -e "${ACCOUNT}" | awk '{print $2}')
+		local CLIENT_SECRET=$(echo -e "${ACCOUNT}" | awk '{print $3}')
+		local REFESH_TOKEN=$(echo -e "${ACCOUNT}" | awk '{print $4}')
+		
+		echo -n "\"CLIENT_ID${NAME}\": \"${CLIENT_ID}\","
+		echo -n "\"CLIENT_SECRET${NAME}\": \"${CLIENT_ID}\","
+		echo -n "\"REFESH_TOKEN${NAME}\": \"${CLIENT_ID}\","
+	done
 }
 
 function hmac256_py() {
@@ -55,8 +59,12 @@ EOF
 }
 
 auth_sign() {
+	local SECRETID=${SECRET_ID}
+	local SECRETKEY=${SECRET_KEY}
 	local HMAC256="$(mktemp)"
 	local ENTER=$'\n'
+	local TIME=$1
+	local DATE=$(date -d @${TIME} +%F)
 
 	# ************* 步骤 1：拼接规范请求串 *************
 
@@ -87,24 +95,103 @@ ${ENTER}${HASHED_REQUEST}"
 	 'SignedHeaders='${SIGED_HEADERS}, Signature=${SIGNATURE}
 }
 
-post_result() {
+post_result_func_trigger() {
+	ACTION=$1
+	TRIGGER_NAME=$2
+	TRIGGER_DESC=$3
+	TIME=$(timestamp 1)
+	REGION=${TENCENTCLOUD_REGION}
+	VER='2018-04-16'
+	ALGORITHM='TC3-HMAC-SHA256'
+	TYPE="Content-Type:application/json"
+	HOST='scf.tencentcloudapi.com'
+	BODY="{\"FunctionName\": \"${SCF_FUNCTIONNAME}\", \
+	\"TriggerName\": \"${TRIGGER_NAME}\", \
+	\"Type\": \"timer\", \
+	\"TriggerDesc\": \"${TRIGGER_DESC}\"}"
+
 	curl -s \
-    -H "Host: ${HOST}" \
+	-H "Host: ${HOST}" \
 	-H "X-TC-Action: ${ACTION}" \
 	-H "X-TC-Timestamp: ${TIME}" \
 	-H "X-TC-Version: ${VER}" \
 	-H "X-TC-Region: ${REGION}" \
 	-H "X-TC-Language: zh-CN" \
 	-H "${TYPE}" \
-	-H "Authorization: $(auth_sign)" \
+	-H "Authorization: $(auth_sign ${TIME})" \
 	-d "${BODY}" "https://${HOST}/"
 }
 
-main() {
-	DATE=$(timestamp 2)
+
+post_result_func() {
+	ACTION=$1
+	FUNC_NAME=$2
+	ZIPFILE=$3
+	HANDLER='index.main_handler'
+	REGION='ap-shanghai'
+	RUNTIME='CustomRuntime'
+	CODE='ZipFile'
+	MEM=64
+	TIMEOUT=60
+
 	TIME=$(timestamp 1)
-	post_result
+	VER='2018-04-16'
+	ALGORITHM='TC3-HMAC-SHA256'
+	TYPE='Content-Type:application/json'
+	HOST='scf.tencentcloudapi.com'
+
+	BODY_JSON=/tmp/body.json
+
+	if [[ ${ACTION} == 'CreateFunction' ]]; then
+		echo -e "{\"FunctionName\": \"${FUNC_NAME}\", \
+		\"Runtime\": \"${RUNTIME}\", \
+		\"MemorySize\": ${MEM}, \
+		\"Handler\": \"${HANDLER}\", \
+		\"Timeout\": ${TIMEOUT}, \
+		\"Code\": {\"${CODE}\": \"${ZIPFILE}\"}}" \
+		>${BODY_JSON}
+
+	elif [[ ${ACTION} == 'UpdateFunctionCode' ]]; then
+		echo -e "{\"FunctionName\": \"${FUNC_NAME}\", \
+		\"Handler\": \"${HANDLER}\", \
+		\"Code\": {\"${CODE}\": \"${ZIPFILE}\"}}" \
+		>${BODY_JSON}
+
+	elif [[ ${ACTION} == 'DeleteFunction' ]]; then
+		echo -e "{\"FunctionName\": \"${FUNC_NAME}\"}" \
+		>${BODY_JSON}
+
+	elif [[ ${ACTION} == 'Invoke' ]]; then
+		echo -e "{\"FunctionName\": \"${FUNC_NAME}\"}" \
+		>${BODY_JSON}
+	else
+		echo "参数错误！"
+		echo -e "此脚本仅支持: \\n\
+		CreateFunction	创建函数\\n\
+		UpdateFunctionCode	更新函数代码\\n\
+		DeleteFunction	删除函数\\n\
+		Invoke	运行函数\\n"
+	fi
+
+	BODY=$(cat ${BODY_JSON})
+
+	curl -s \
+	-H "Host: ${HOST}" \
+	-H "X-TC-Action: ${ACTION}" \
+	-H "X-TC-Timestamp: ${TIME}" \
+	-H "X-TC-Version: ${VER}" \
+	-H "X-TC-Region: ${REGION}" \
+	-H "X-TC-Language: zh-CN" \
+	-H "${TYPE}" \
+	-H "Authorization: $(auth_sign ${TIME})" \
+	-d @${BODY_JSON} \
+	"https://${HOST}/"
+
+	rm -f ${BODY_JSON}
 }
 
-main
+# post_result_func DeleteFunction mytest
 
+# zip -r /tmp/tencent_cloud_auto_api_call.zip ./ -x ".git/*" -x ".github/*"
+# post_result_func UpdateFunctionCode mytest $(cat ./tencent_cloud_auto_api_call.zip | base64 -w 0) 
+# rm -rf /tmp/tencent_cloud_auto_api_call.zip
