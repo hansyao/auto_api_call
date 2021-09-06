546cede9371f1fe2aea82473d27fb0a894d9aab2
hansyao <hansyow@gmail.com>
2021-09-06 10:38:03 +0800

    add apply to remote server

--- graph_api_app.sh
+++ graph_api_app.sh
@@ -217,7 +217,7 @@ function update_cron() {
 		./trigger.sh 'CreateTrigger' "${SCF_FUNCTIONNAME}" 'graph_api' "${CRON}"
 	# VPS自动任务
 	elif [[ $[PLATFORM] -eq 3 ]]; then
-		crontab -l | { cat; echo -e "${M} ${H} * * * * source ${HOME}/.bashrc && cd graph_api && ./graph_api_app.sh"; } | crontab -
+		crontab -l | { cat; echo -n "${M} ${H} * * * * source ${HOME}/.bashrc && cd graph_api && ./graph_api_app.sh"; } | crontab -
 	else
 		echo -e "platform 参数不正确!"
 		return 1
